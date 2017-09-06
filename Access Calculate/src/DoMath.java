98159137d7e941ef7fb36063a757526b3cdf17f3
pyang329 <31467616+pyang329@users.noreply.github.com>
2017-09-06 11:33:38 -0700

    Calculate Library Square

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,11 @@
+// Pauline Yang
+//September 6 2017
+//this is the runner for the calculate library
+//it is used to test the methods and calculate
+
+public class DoMath {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		System.out.println(Calculate.square(-7));
+	}
+}
---
+++ Calculate Library/src/Calculate.java
@@ -0,0 +1,20 @@
+// Pauline Yang
+//September 6 2017
+//our self written math library
+// it cointains a series of methods to do math functions
+public class Calculate {
+	//square the input
+	public static int square (int operand) {
+		int squared = operand*operand;
+		return squared;
+	}
+}	
+	
+/*	public static int cube (int operand) {
+		int cubed = operand*operand*operand;
+		return cubed;
+	}
+	
+	public static int averageReturn (double x, double y)
+		
+}*/
