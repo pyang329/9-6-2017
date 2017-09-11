c65754a51fadc1cc47f5765746810a400f725367
pyang329 <31467616+pyang329@users.noreply.github.com>
2017-09-11 01:54:40 -0700

    9/11 finished calculate library pt2

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -15,5 +15,10 @@ public class DoMath {
 		System.out.println(Calculate.toImproperFrac(3, 1, 2));
 		System.out.println(Calculate.toMixedNum(7, 2));
 		System.out.println(Calculate.foil(2, 3, 6, -7, "n"));
+		System.out.println(Calculate.isDivisibleBy(3, 1));
+		System.out.println(Calculate.absValue(1.5));
+		System.out.println(Calculate.max(3, 6, 2));
+		System.out.println(Calculate.min(3, 2));
+		System.out.println(Calculate.round2(3.6677));
 	}
 }
