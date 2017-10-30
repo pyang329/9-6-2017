5a71f06a82e425c735eed0fac75560021d0f3790
pyang329 <31467616+pyang329@users.noreply.github.com>
2017-10-30 09:44:51 -0700

    10/30 work

--- Split String/src/Split.java
+++ Split String/src/Split.java
@@ -21,40 +21,70 @@ public class Split {
 		
 		
 		System.out.println(Arrays.toString("I really like red apples".split("really")));
-		System.out.println(splitSandwichAtBread("applespineapplesbreadlettustomatobaconmayohambreadcheese"));
-
+		System.out.println(splitAtBread("applespineapplesbreadlettustomatobaconmayohambreadcheese"));
+		System.out.println(splitAtBreadWithSpaces("apples pineapples bread lettus tomato bacon mayo ham bread cheese"));
+		System.out.println(splitAtBread("apples pineapples bread lettus tomato bacon mayo ham bread cheese"));
 	}
-	
-	public static String splitSandwichAtBread (String sandwich) {
-		String finalInput = "";
-		String ingredients = " " + sandwich + " ";
-		String [] sandwichInnards = sandwich.split(" ");
-		for (int i=0; i<sandwichInnards.length; i++) {
-			ingredients += sandwichInnards[i];
-		}
-		String [] arraySandwichHolder = ingredients.split("bread");
-		if(arraySandwichHolder.length <= 2) {
-			System.out.println("Ingredients are not in a sandwich.");
+
+	//Your task Part 1:
+	/*Write a method that take in a string like 
+	* "applespineapplesbreadlettustomatobaconmayohambreadcheese"
+	*describing a sandwich
+	* use String.split to split up the sandwich by the word "bread" and 
+	* return what's in the middle of the sandwich and ignores what's on the outside
+	* What if it's a fancy sandwich with multiple pieces of bread?
+	*/
+	public static String splitAtBread(String sandwichInput){
+		String finalOutput = "";
+		String Ingredients = " " + sandwichInput + " ";
+		String[] arrayBread = Ingredients.split("bread");
+		if(arrayBread.length <= 2){
+			System.out.println("Ingredients entered are not in a sandwich.");
 		}else{
-			for (int i=1; i<arraySandwichHolder.length-1; i++) {
-				if(arraySandwichHolder[i].equals("")  &&  i == arraySandwichHolder.length -2) {
-					System.out.println("Ingredients are not in a sandwich");
-				}else {
-					finalInput += arraySandwichHolder[i];
+			for (int i = 1; i < arrayBread.length-1; i++){
+				String middleOfSandwich = arrayBread[i];
+				if(arrayBread[i].equals("")){
+					System.out.print("");
+				}else{
+					finalOutput += middleOfSandwich;
 				}
 			}
-			
 		}
-		return finalInput;
+		return finalOutput;
 	}
-}
 
-//Your task Part 1:
-/*Write a method that take in a string like 
-* "applespineapplesbreadlettustomatobaconmayohambreadcheese"
-*describing a sandwich
-* use String.split to split up the sandwich by the word "bread" and 
-* return what's in the middle of the sandwich and ignores what's on the outside
-* What if it's a fancy sandwich with multiple pieces of bread?
-*/
+	//Your task Part 2:
+		/*Write a method that take in a string like "apples pineapples bread lettus tomato bacon mayo ham bread cheese" describing a sandwich use String.split to split up the sandwich at the spaces, " ", and 
+		* return what's in the middle of the sandwich and ignores what's on the outside
+		* Again, what if it's a fancy sandwich with multiple pieces of bread?
+		*/	
+	public static String splitAtBreadWithSpaces(String sandwichInput){
+		String finalOutput = "";
+		String ingredients = " " + sandwichInput + " ";
+		String [] sandwichInnards = ingredients.split(" ");
+		for (int i = 0; i < sandwichInnards.length; i++){
+			ingredients += sandwichInnards[i];
+		}
+		String[] arrayBread = ingredients.split("bread");
+		if(arrayBread.length <= 2){
+			System.out.println("Ingredients entered are not in a sandwich.");
+		}else {
+			for (int i = 1; i < arrayBread.length-1; i++){
+				String middleOfSandwich = arrayBread[i];
+				if (arrayBread[i].equals("") && i == arrayBread.length - 2){
+					System.out.println("Ingredients entered are not in a sandwich.");
+				}else {
+					finalOutput += middleOfSandwich;
+				}
+			}
+		}
+		return finalOutput;
+		}
+}	
+
+
+
+
+
+
 
