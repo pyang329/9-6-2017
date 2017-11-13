8d12f9dd27c146cd2a0f626b76fe7ca1fdbd23c7
pyang329 <31467616+pyang329@users.noreply.github.com>
2017-11-13 09:51:59 -0800

    11/13/17 work

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -2,9 +2,9 @@ package fracCalc;
 
 public class FracCalc {
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         // TODO: Read the input from the user and call produceAnswer with an equation
+    	System.out.println(produceAnswer("1_2/3 + 4_5/6"));
 
     }
     
@@ -16,11 +16,14 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
+    public static String produceAnswer(String input){ 
         // TODO: Implement this function to produce the solution to the input
-        
-        return "";
+       // String [] inputString = new String [input.length()];
+        String [] separateIntoParts = input.split(" ");
+        String firstOperand = separateIntoParts[0];
+        String operator = separateIntoParts[1];
+        String secondOperand = separateIntoParts[2];
+        return secondOperand;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
