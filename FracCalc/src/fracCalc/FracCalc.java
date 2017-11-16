2c22841cdaa1154c3f2ddca0ed05118f4147e20c
pyang329 <31467616+pyang329@users.noreply.github.com>
2017-11-16 09:41:13 -0800

    11/16/17 work

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,10 +1,18 @@
+// Pauline Yang
 package fracCalc;
-
+import java.util.Arrays;
+import java.util.Scanner; 
 public class FracCalc {
 
     public static void main(String[] args) {
         // TODO: Read the input from the user and call produceAnswer with an equation
-    	System.out.println(produceAnswer("1_2/3 + 4_5/6"));
+    		String input;
+    		Scanner sc = new Scanner(System.in);
+    		do {
+    			System.out.println("Please enter in your input. Type quit to exit.");
+    			input = sc.nextLine();
+    			System.out.println(produceAnswer(input));
+    		} while (input != "quit");
 
     }
     
@@ -23,9 +31,47 @@ public class FracCalc {
         String firstOperand = separateIntoParts[0];
         String operator = separateIntoParts[1];
         String secondOperand = separateIntoParts[2];
-        return secondOperand;
+        int[] parsedFirstOperand = parseOperand(firstOperand);
+		int[] parsedSecondOperand = parseOperand(secondOperand);
+		String whole = "whole:" + parsedSecondOperand[0];
+		String numerator = " numerator:" + parsedSecondOperand[1];
+		String denominator = " denominator:" + parsedSecondOperand[2];
+        return whole + numerator + denominator;
     }
+    
 
     // TODO: Fill in the space below with any helper methods that you think you will need
+    public static int [] parseOperand (String operand) {
+    		String[] wholeNumber = operand.split("_");
+		String[] splitFraction = wholeNumber[1].split("/");
+		String[] threeDigitsSort = {wholeNumber[0], splitFraction[0], splitFraction[1]};
+		if (splitFraction[0] == "0") {
+			splitFraction [1] = "1";
+		}
+		int[] intsInOrder = new int[3];
+	    for (int i=0; i < threeDigitsSort.length; i++) {
+	    		intsInOrder[i] = Integer.parseInt(threeDigitsSort[i]);
+	    }
+	    return intsInOrder;
+    }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
